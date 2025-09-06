a87af2f0d0e0418ac3912b9ce1e9c3b056d6ca2e
Natthaphon Khuankuntod <nkuankuntod.0896754371@gmail.com>
2025-09-06 20:34:57 +0700

    update middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,11 @@
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-import { verifyToken } from "./lib/jwt";
-
 
 export default async function middleware(req) {
   const token = req.cookies.get("token")?.value;
 
   if (!token) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    return NextResponse.redirect(new URL("/login", req.url));
   }
-
 
   return NextResponse.next();
 }
